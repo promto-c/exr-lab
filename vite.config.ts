3c138b84e1eca95f56b8eafb730874bdb85d53e2
promto-c <prommm01@gmail.com>
2026-02-16 04:25:01 +0700

    Configure GitHub Pages deployment from main

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
     return {
+      base: mode === 'production' ? '/exr-lab/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
